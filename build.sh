e6f7f246859b0dc6fe99c15ffa42682d8263d3fb
Yannick Weiss <contact@yannickweiss.com>
2016-08-07 20:05:02 +0200

    adding empty token file

--- build.sh
+++ build.sh
@@ -1,10 +1,12 @@
+#!/bin/sh
+APIKEY=$(<apikey.txt)
 git push origin master
 mkdir build
 VERSION=$(/usr/libexec/PlistBuddy -c "Print CFBundleShortVersionString" "Info.plist")
 xcodebuild -project DisplayBrightness.xcodeproj -scheme DisplayBrightness -archivePath build/DisplayBrightness.xarchive archive
 xcodebuild -exportArchive -archivePath build/DisplayBrightness.xarchive.xcarchive -exportPath build/ -exportOptionsPlist export.plist
-zip -r "DisplayBrightness_${VERSION}.zip" build/DisplayBrightness.app/
-rm -r build
+cd build
+zip -r "DisplayBrightness_${VERSION}.zip" DisplayBrightness.app/
 # https://developer.github.com/v3/repos/releases/#create-a-release 
 
 
