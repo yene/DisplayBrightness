dea242d8fb47138b354c588f4dbe7685c42d2d9f
Yannick Weiss <contact@yannickweiss.com>
2016-08-07 18:58:05 +0200

    Increased version to 1.7.3

--- build.sh
+++ build.sh
@@ -1,5 +1,10 @@
+git push origin master
 mkdir build
+VERSION=$(/usr/libexec/PlistBuddy -c "Print CFBundleShortVersionString" "Info.plist")
 xcodebuild -project DisplayBrightness.xcodeproj -scheme DisplayBrightness -archivePath build/DisplayBrightness.xarchive archive
 xcodebuild -exportArchive -archivePath build/DisplayBrightness.xarchive.xcarchive -exportPath build/ -exportOptionsPlist export.plist
-zip -r DisplayBrightness.zip build/DisplayBrightness.app/
+zip -r "DisplayBrightness_${VERSION}.zip" build/DisplayBrightness.app/
 rm -r build
+# https://developer.github.com/v3/repos/releases/#create-a-release 
+
+
